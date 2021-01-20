833090f7a5c6157b52c1d241f307db28525a97b3
lutfiqaraman <lutfiqaraman@gmail.com>
2021-01-20 16:24:26 +0200

    render an index page

---
+++ public/js/renderData.js
@@ -0,0 +1,1 @@
+// Render data using javascript
--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import path from 'path';
+import pug from 'pug';
 
 const app = express();
 
+//Setup pugs engine and views location
+const dirname = path.resolve();
+app.set('views', dirname + '/views');
+app.set('view engine', 'pug');
+
 // Render home page
 app.get('/', (req, res) => {
-    res.send('here you go ...');
+    res.render('index', {title: 'Home Page', message: 'Breakingbad App'});
 });
 
 // Run a nodejs Server
 app.listen(3000, () => {
+    console.log(dirname);
     console.log('server is running');
 });

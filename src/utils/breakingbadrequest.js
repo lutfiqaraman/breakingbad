50ebafed61211103cd34bddb31a3cb15b1e4159b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-02-16 21:54:14 +0200

    Get information from Breaking bad API

--- src/utils/breakingbadrequest.js
+++ src/utils/breakingbadrequest.js
@@ -1,2 +1,31 @@
+import * as https from 'https';
+
 const baseURL = 'https://www.breakingbadapi.com/api/characters?name=Walter';
-console.log(baseURL);
+
+async function requestPromise(path) {
+    return new Promise((resolve, reject) => {
+        https.get(path, (resp) => {
+            let data = '';
+
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            resp.on('end', () => {
+                resolve(data);
+            });
+
+        }).on("error", (error) => {
+            reject(error);
+        });
+    });
+}
+
+(async function () {
+    try {
+        const result = await requestPromise(baseURL);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+})();

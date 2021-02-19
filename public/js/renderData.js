e1cbeb0ce89e89d87414adf08a9f12e44338ca63
lutfiqaraman <lutfiqaraman@gmail.com>
2021-02-19 19:59:34 +0200

    fetch data from Breaking Bad API

--- public/js/renderData.js
+++ public/js/renderData.js
@@ -1,1 +1,17 @@
 // Render data using javascript
+const searchForm = document.querySelector("form");
+const searchBox  = document.querySelector("input");
+
+searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const characterName = searchBox.value;
+
+    fetch("")
+        .then()
+        .catch(error => {
+            if (error)
+                console.log(error);
+        });
+})
+
+
